f7ea251272af6153da10afda0e395c0d522affd7
lulu.chen <lulu.chen@linecorp.com>
2022-08-23 02:12:11 +0800

    add timer

---
+++ src/index.js
@@ -0,0 +1,31 @@
+document.getElementById("app").innerHTML = `
+<h1>Timer</h1>
+<div id="count_down_timer"></div>
+`;
+
+const today = new Date();
+const andyThirtyBirthay = new Date("2024-4-23");
+let countDownTime = andyThirtyBirthay - today;
+
+function countDownTimer() {
+  setInterval(() => {
+    countDownTime = countDownTime - 1000;
+    const second = 1000;
+    const minute = second * 60; // 6000
+    const hour = minute * 60; // 36000
+    const day = hour * 24; // 864000
+
+    const days = Math.floor(countDownTime / day);
+    const hours = Math.floor((countDownTime - days * day) / hour);
+    const minutes = Math.floor(
+      (countDownTime - (days * day + hours * hour)) / minute
+    );
+    const seconds = Math.floor(
+      (countDownTime - (days * day + hours * hour + minutes * minute)) / second
+    );
+    document.getElementById("count_down_timer").innerHTML =
+      days + "天" + hours + ":" + minutes + ":" + seconds;
+  }, 1000);
+}
+
+countDownTimer();
